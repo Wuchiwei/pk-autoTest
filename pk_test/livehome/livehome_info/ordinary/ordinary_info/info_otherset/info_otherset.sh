68957b929f489c77f2fff533bc299f96e44bf574
chang teddy <cloveradd@gmail.com>
2017-10-26 17:03:24 +0800

    ordinary_info/info_otherset/ : Add new test case in info_otherset/

---
+++ pk_test/livehome/livehome_info/ordinary/ordinary_info/info_otherset/info_otherset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python ordinary_otherset_case1.py
+python ordinary_otherset_case2.py
+python ordinary_otherset_case3.py
+python ordinary_otherset_case4.py
+python ordinary_otherset_case5.py
+python ordinary_otherset_case6.py
+python ordinary_otherset_case7.py
---
+++ pk_test/livehome/livehome_info/ordinary/ordinary_info/info_otherset/ordinary_otherset_case6.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+import sys
+sys.path.append("../../../../../../import")
+import setup
+import element
+from random import randint
+from appium import webdriver
+from time import sleep
+
+class SimpleIOSTests(unittest.TestCase):
+
+    def setUp(self):
+        # set up appium
+        setup.load_for6(self)
+        setup.ck_login_ordinary(self)
+        sleep(1)
+
+    def test_ordinary_otherset_case6(self):#原先密碼與新密碼皆未輸入
+        element.livehome_element(self)
+        self.info.click()
+        sleep(2)
+        element.livehome_info(self)
+        self.other_set.click()
+        sleep(1)
+        element.other_set(self)
+        self.rule_butten.click()
+        sleep(1)
+        self.driver.find_element_by_accessibility_id('用戶規章')
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(SimpleIOSTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
